a9fcf37838551ace1ae6a2a10025dc732aa6e639
Alisson Campos <39539326+alissongla@users.noreply.github.com>
2022-01-08 14:54:22 -0300

    Update dias-da-semana.ts

--- app/enums/dias-da-semana.ts
+++ app/enums/dias-da-semana.ts
@@ -1,9 +1,9 @@
 export enum DiasDaSemana {
-    DOMING0,
-    SEGUNDA,
-    TERCA,
-    QUARTA,
-    QUINTA,
-    SEXTA,
-    SABADO,
+    DOMING0 = 0,
+    SEGUNDA = 1,
+    TERCA = 2,
+    QUARTA = 3,
+    QUINTA = 4,
+    SEXTA = 5,
+    SABADO = 6,
 }
